53bef7b6ed05c684e3b038574f08b4403cb22ff8
Anil703K <karingulaanilgoud437@gmail.com>
2025-07-05 21:18:15 +0530

    Fix remixicon import path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './index.css';
 import App from './App';
+import 'remixicon/css/remixicon.css';
 import { createRoot } from 'react-dom/client';
 const root = createRoot(document.getElementById('root'));
 root.render(<App/>);
